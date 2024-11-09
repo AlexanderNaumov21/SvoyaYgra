ea308123c3ec59036c4b5e2dc192247053534969
AlexanderNaumov21 <99212015+AlexanderNaumov21@users.noreply.github.com>
2024-11-09 17:40:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Начать игру</title>
+    <link rel="stylesheet" href="glavn.css">
+</head>
+<body>
+    <div class="overlay">
+        <h1 class="title"></h1>
+        <a href="pravila.html" class="start-btn">Начать игру</a>
+    </div>
+</body>
+</html>
---
+++ tablitsa.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Своя игра</title>
+    <link rel="stylesheet" href="oform.css">
+</head>
+<body>
+    <div class="container">
+        <table>
+            <tr>
+                <th>ТЕРМИНЫ</th>
+                <td><a href="question1.html" class="question-btn">100</a></td> <!-- Информационные правоотношения -->
+                <td><a href="question2.html" class="question-btn">200</a></td> <!-- Уникальный адрес веб сайта -->
+                <td><a href="question3.html" class="question-btn">300</a></td> <!-- Захват доменов с целью последующей перепродажи -->
+                <td><a href="question4.html" class="question-btn">400</a></td> <!-- Субъекты правоотношений в Интернете -->
+                <td><a href="question5.html" class="question-btn">500</a></td> <!-- Авторское право -->
+            </tr>
+            <tr>
+                <th>ДА/НЕТ</th>
+                <td><a href="question6.html" class="question-btn">100</a></td> <!--Нельзя. В любой социальной сети, даже запрещённой на территории России, права авторов защищаются законом.-->
+                <td><a href="question7.html" class="question-btn">200</a></td> <!-- Нет, сайт охраняется авторскими правами, ведь это тоже программное обеспечение -->
+                <td><a href="question8.html" class="question-btn">300</a></td> <!-- 10 -->
+                <td><a href="question9.html" class="question-btn">400</a></td> <!-- Доменное имя защищается законом ТОЛЬКО, в случае если оно зарегистрировано как товарный знак. -->
+                <td><a href="question10.html" class="question-btn">500</a></td> <!-- да -->
+            </tr>
+            <tr>
+                <th>ЗАКОНЫ</th>
+                <td><a href="question11.html" class="question-btn">100</a></td> <!-- Федеральный закон «Об информации, информационных технологиях и о защите информации»  -->
+                <td><a href="question12.html" class="question-btn">200</a></td> <!-- Федеральный закон "О персональных данных" (№ 152-ФЗ), Федеральный закон "Об информации, информационных технологиях и о защите информации" (№ 149-ФЗ) и Трудовой кодекс РФ.  -->
+                <td><a href="question13.html" class="question-btn">300</a></td> <!-- Федеральный закон № 152-ФЗ «О персональных данных» -->
+                <td><a href="question14.html" class="question-btn">400</a></td> <!-- Трудовой кодекс -->
+                <td><a href="question15.html" class="question-btn">500</a></td> <!-- ФЗ "О средствах массовой информации" -->
+            </tr>
+            <tr>
+                <th>ЦИФРЫ</th>
+                <td><a href="question16.html" class="question-btn">100</a></td> <!-- 300 000 рублей -->
+                <td><a href="question17.html" class="question-btn">200</a></td> <!-- от 50 000 до 300 000 -->
+                <td><a href="question18.html" class="question-btn">300</a></td> <!-- 2018  -->
+                <td><a href="question19.html" class="question-btn">400</a></td> <!-- 2015 -->
+                <td><a href="question20.html" class="question-btn">500</a></td> <!--Закон регулировал только обмен физическими объектами, но не учитывал развитие мировых коммуникационных сетей и информационного обмена. В условиях стремительного роста цифровых технологий он перестал соответствовать реальности. -->
+            </tr>
+            <tr>
+                <th>ГОС ОРГАНЫ</th>
+                <td><a href="question21.html" class="question-btn">100</a></td> <!-- Роскомнадзор  -->
+                <td><a href="question22.html" class="question-btn">200</a></td> <!-- вопроса нет -->
+                <td><a href="question23.html" class="question-btn">300</a></td> <!-- Министерство юстиции   -->
+                <td><a href="question24.html" class="question-btn">400</a></td> <!-- С помощью знака авторских прав © -->
+                <td><a href="question25.html" class="question-btn">500</a></td> <!-- Мосгорсуд  -->
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
+
